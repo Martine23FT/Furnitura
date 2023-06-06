f8d1703b49be306495517f9c9f8ae586dfe59e95
Martine23FT <martinemidttveit@hotmail.com>
2023-06-06 17:12:25 +0200

    Changes
    Added a slideshow.

--- script.js
+++ script.js
@@ -60,3 +60,23 @@ buttonColor ("size3")
 buttonColor ("size4")
 
 ///// Possible to make a if / else for the quantity, with in stock and not in stock. 
+
+var slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slideshow-container")[0].getElementsByTagName("img");
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 2000);
+}
